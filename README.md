cc9d19a2f9d703627a42c43618daa82a09a206fa
Sashitra <sashisunil@gmail.com>
2021-08-26 08:27:02 -0700

    Basic Setup done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DockerDemo
 This repository is created to Docker Image and container to implement CICD
+## Create Repo
+## Clone the Repo
+## Springboot setup
+##connect to MySql
+##Run the application
---
+++ src/main/java/com/tekarch/dockerdemoaug23/Dockerdemoaug23Application.java
@@ -0,0 +1,13 @@
+package com.tekarch.dockerdemoaug23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dockerdemoaug23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dockerdemoaug23Application.class, args);
+	}
+
+}
